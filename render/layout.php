3db2d16ad331ecdb54421e649f33267c6fdcaa9f
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-05-13 01:42:37 +0200

    Update layout rendering to use slots for title and content

--- render/layout.php
+++ render/layout.php
@@ -3,12 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= implode(' | ', slot('title')); ?></title>
 </head>
 <body>
-    
-<?php
-echo implode(slot('content'));
-?>
+    <?= implode(slot('content')); ?>
 </body>
 </html>
