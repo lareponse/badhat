44e71fabbbcae9ec923beee9d8f687b5b4b85071
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-05-16 03:13:10 +0200

    scaffold is now a view, vd() has location

---
+++ add/bad/scaffold.php
@@ -0,0 +1,15 @@
+<h1>Missing route: <?= $path ?></h1>
+<strong>Choose route file to create:</strong>
+<dl>
+<?php
+foreach ($candidates as $depth => $response) {
+    $handler = $response['handler'];
+    $handlerArgs = empty($response['args']) ? 'none' : implode(',', $response['args']);
+    $templateCode = "<?php\nreturn function (...\$args) {\n\t// Expected arguments: function($handlerArgs)\n\treturn ['status' => 200, 'body' => __FILE__];\n};";
+    ?>
+    <dt><strong><?= htmlspecialchars($handler) ?></strong></dt>
+    <dd><pre><?= htmlspecialchars($templateCode) ?></pre></dd>
+    <?php
+}
+?>
+</dl>
