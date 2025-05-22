76b2cd11b6b34fc07e21462e3c0c338a60c9436e
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-05-22 15:33:11 +0200

    Add database connection setup in index.php and adjust route path

--- public/index.php
+++ public/index.php
@@ -2,10 +2,17 @@
 
 require '../add/core.php';
 require '../add/bad/dev.php';
+require '../add/bad/db.php';
 require '../add/bad/ui.php';
 require '../add/bad/security.php';
 
-$route_root = realpath(__DIR__ . '/../app/io/route');
+list($dsn, $u, $p) = require '../app/data/credentials.php';
+// $dsn = null;
+db($dsn, $u, $p, [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+]);
+
+$route_root = __DIR__ . '/../app/io/route';
 $route = route($route_root);
 $response = handle($route);
 respond($response);
