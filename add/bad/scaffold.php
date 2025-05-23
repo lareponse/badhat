a6729f1380aab9c8513e53fdcd7a36c5777fbabe
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-05-23 23:20:38 +0200

    actionable scaffold.php output

--- add/bad/scaffold.php
+++ add/bad/scaffold.php
@@ -1,30 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Scaffold</title>
 </head>
 
 <body>
-    <h1>Missing route: <?= request()['path'] ?></h1>
-    <strong>Choose route file to create:</strong>
-    <dl>
-        <?php
-        foreach (io_candidates(request()['route_root'], true) as $depth => $response) {
-            $handler = $response['handler'];
-            $handlerArgs = empty($response['args']) ? 'none' : implode(',', $response['args']);
-            $templateCode = "<?php\nreturn function (...\$args) {\n\t// Expected arguments: function($handlerArgs)\n\treturn ['status' => 200, 'body' => __FILE__];\n};";
-        ?>
-            <dt><strong><?= htmlspecialchars($handler) ?></strong></dt>
-            <dd>
-                <pre><?= htmlspecialchars($templateCode) ?></pre>
-            </dd>
-        <?php
-        }
-        ?>
-    </dl>
+    <h1>Missing end point <?= request()['path'] ?></h1>
+    <span>Choose route file to create in: <strong><?= request()['route_root'] ?>/</strong></span>
+
+        <dl>
+            <?php
+            foreach (io_candidates(request()['route_root'], true) as $depth => $response) {
+                $handler = $response['handler'];
+                $handlerArgs = empty($response['args']) ? 'none' : implode(',', $response['args']);
+                $templateCode = "<?php\n// Expected arguments: $handlerArgs\nreturn function (...\$args) {\n\treturn ['status' => 200, 'body' => __FILE__];\n};";
+            ?>
+                <dt><strong><?= htmlspecialchars(str_replace(request()['route_root'], '', $handler)) ?></strong></dt>
+                <dd>
+                    <pre><?= htmlspecialchars($templateCode) ?></pre>
+                </dd>
+            <?php
+            }
+            ?>
+        </dl>
 </body>
 
 </html>
