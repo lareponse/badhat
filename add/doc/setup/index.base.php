6d5f8ed4401deb420f66740df107d6fa3d38e6b7
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-05-27 02:18:33 +0200

    Refactor authentication backend files and update documentation for clarity

--- add/doc/setup/index.base.php
+++ add/doc/setup/index.base.php
@@ -4,8 +4,8 @@ require '../add/core.php';
 require '../add/bad/dev.php';
 require '../add/bad/db.php';
 require '../add/bad/ui.php';
-require '../add/bad/security.php';
-require '../add/bad/auth_sql.php';
+require '../add/bad/guard_auth.php';
+require '../add/bad/auth_backend_sql.php';
 
 list($dsn, $u, $p) = require '../app/data/credentials.php';
 // $dsn = null;
