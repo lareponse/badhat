a8cb90dba04a20944e453877839e304d9515aca8
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-03 03:00:34 +0200

    refactor: streamline IO functions and enhance closure handling

--- add/core.php
+++ add/core.php
@@ -1,30 +1,16 @@
 <?php
+
 declare(strict_types=1);
 
 require 'add/io.http.php';
 require 'add/io.file.php';
 
-function follow(array $quest, array $request): array
-{
-    foreach ($quest['prepare'] as $prepare) {
-        $quest += $prepare['closure']($quest, $request);
-    }
-
-    if (isset($quest['execute']['closure']) && is_callable($quest['execute']['closure'])) {
-        $quest += $quest['execute']['closure']($quest, $request, ...$quest['execute']['args'] ?? []);
-    }
-
-    foreach ($quest['conclude'] as $conclude) {
-        $quest += $conclude['closure']($quest, $request);
-    }
-
-    return $quest;
-}
-
-function deliver($quest, array $request): array
+function deliver($quest): array
 {
     // vd($quest, 'deliver()');
-    $view = io_mirror($quest);
+    $in = io();
+    $out = io_out($in);
+    $view =  str_replace($in, $out, $quest['execute']['handler']);
     $html = render($quest, $view);
     return http_response(200, $html, ['Content-Type' => 'text/html; charset=UTF-8']);
 }
