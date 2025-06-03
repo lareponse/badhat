e6a292fcfd4fb679ad77b69ffd48f997c32e7f9f
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-03 12:58:35 +0200

    fixed handler retrieval in deliver function

--- add/bad.php
+++ add/bad.php
@@ -10,7 +10,7 @@ function deliver($quest)
     // vd($quest, 'deliver()');
     $in = io();
     $out = io_out($in);
-    $view =  str_replace($in, $out, $quest['execute']['handler']);
+    $view =  str_replace($in, $out, key($quest['execute']));
     $html = render($quest, $view);
     return http_response(200, $html, ['Content-Type' => 'text/html; charset=UTF-8']);
 }
