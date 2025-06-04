4d299dceef6192a0a655d9922402c8ac4bb0209e
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-05 01:42:11 +0200

    io_map is relative, no need to run twice

--- add/bad/io.php
+++ add/bad/io.php
@@ -1,11 +1,14 @@
 <?php
 
-function io(string $in, ?string $out=null)
+function io(string $in, ?string $out = null)
 {
     $out ??= io_other($in); // convention costs, use $out
 
-    $log = io_run(vd(io_map($in)));
-    $see = io_run(io_map($out));
+    $path = parse_url($_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH);
+    $map = vd(io_map($path));
+    $log = (io_run($in, $map));
+    $see = (io_run($out, $map));
+
     if (is_array($see) && is_array($log))
         array_unshift($log, ...$see);
     elseif (is_array($log))
@@ -14,39 +17,33 @@ function io(string $in, ?string $out=null)
     return $log;
 }
 
-function io_map(string $home, ?string $path = null): array
+function io_map(string $path, ?string $when_empty = 'index'): array
 {
     $map = [];
-    $home = realpath($home) ?: throw new RuntimeException('Home is not real', 500);
-    $path ??= parse_url($_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH);
+    $segments = (empty($path) || $path === '/') 
+        ? [$when_empty] // default to index if path is empty
+        : explode('/', trim($path, '/'));
+    
+    $cur = '';
+    foreach ($segments as $depth => $seg) {
+        $cur .= DIRECTORY_SEPARATOR . $seg;
+        $args = array_slice($segments, $depth + 1);
 
-    if (empty($path) || $path === '/')
-        $map[] = [$home . DIRECTORY_SEPARATOR . 'index.php'];
-    else {
-        $cur = '';
-        $segments = explode('/', trim($path, '/'));
-        foreach ($segments as $depth => $seg) {
-            $cur .= DIRECTORY_SEPARATOR . $seg;
-            $base_path = $home . $cur;
-            $args = array_slice($segments, $depth + 1);
-
-            if (!empty($seg))  // group match
-                $map[] = [$base_path . DIRECTORY_SEPARATOR . $seg . '.php', $args];
-            $map[] = [$base_path . '.php', $args]; // direct match
-
-        }
-
-        krsort($map); //
+        if (!empty($seg))  // group match
+            $map[] = [$cur . DIRECTORY_SEPARATOR . $seg . '.php', $args];
+        $map[] = [$cur . '.php', $args]; // direct match
     }
+
+    krsort($map); // deep first, keep depth data
 
     return $map;
 }
 
-function io_run($map): ?array
+function io_run($start, $map): ?array
 {
     // vd($map, 'io_run'); // debug output
     foreach ($map as $quest)
-        if ($loot = io_dig(current($quest)))
+        if ($loot = io_dig($start . DIRECTORY_SEPARATOR . current($quest)))
             return [$loot, ...$quest];
 
     return null;
@@ -63,11 +60,11 @@ function io_dig(string $file)
 
 function io_other(string $one)
 {
-    return true                                         // return other child if
+    return (
+        true                                            // return other child if
         && ($io = realpath(dirname($one)))              //      the parent is real
         && ($ios = glob($io . '/*', GLOB_ONLYDIR))      //      the parent has children
-        && isset($ios[0], $ios[1]) && !isset($ios[2])   //      exactly two children
+        && isset($ios[0], $ios[1]) && !isset($ios[2]))  //      exactly two children
         ? ($ios[0] === $one ? $ios[1] : $ios[0])
         : throw new RuntimeException('IO Other Reality Rescinded', 500);
 }
-
