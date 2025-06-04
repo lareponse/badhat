d023922ee7ab933f3ff81645d92eed653891e2c0
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-05 00:43:32 +0200

    MAJOR - io: no nore static, io(in, ?out) to auto run the quest, or io_map > io_run to fiddle + reduce code

--- add/bad/io.php
+++ add/bad/io.php
@@ -1,34 +1,23 @@
 <?php
 
-// first call, routes (in): 
-//      io(route_base)                sets the route base directory, CoC sets render base
-// [OR] io(route_base, render_base)   sets the route base directory, and sets render base
-//
-// subsequent calls:
-//      io()             returns the real path of the current base (route or render)
-//      io(path)         based on URI path, returns map of candidates (call io_map() with current base)
-//      io(null)         switch route base for render base, and vice versa
-function io(?string $io_in = null, ?string $io_out = null)
+function io(string $in, ?string $out=null)
 {
-    static $io = [];
+    $out ??= io_other($in); // convention costs, use $out
 
-    if (empty($io) && $io_in) { // first call, set the bases
-        ($in = realpath($io_in)) ?: throw new RuntimeException('Route Base Reality Rescinded', 500);
-        ($out = realpath($io_out ?: io_other($in))) ?: throw new RuntimeException('Render Base Reality Rescinded', 500);
-        $io = [$in, $out];
-    } else if (func_num_args() === 1 && $io_in === null)
-        $io = array_reverse($io);
+    $log = io_run(io_map($in));
+    $see = io_run(io_map($out));
+    if (is_array($see) && is_array($log))
+        array_unshift($log, ...$see);
+    elseif (is_array($log))
+        array_unshift($log, $see);
 
-    else if (func_num_args() === 1 && $io_in !== null)
-        return io_map(current($io), $io_in);
-
-    return current($io);
+    return $log;
 }
 
-function io_map(string $home, ?string $path = null)
+function io_map(string $home, ?string $path = null): array
 {
     $map = [];
-
+    $home = realpath($home) ?: throw new RuntimeException('Home is not real', 500);
     $path ??= parse_url($_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH);
 
     if (empty($path) || $path === '/')
@@ -53,8 +42,9 @@ function io_map(string $home, ?string $path = null)
     return $map;
 }
 
-function io_try($map): ?array
+function io_run($map): ?array
 {
+    // vd($map, 'io_run'); // debug output
     foreach ($map as $quest)
         if ($loot = io_dig(current($quest)))
             return [$loot, ...$quest];
@@ -65,29 +55,19 @@ function io_try($map): ?array
 function io_dig(string $file)
 {
     ob_start();
-    $callable = @include $file;
-    $content = trim(ob_get_clean()); // significant whitespaces are in tags
+    $callable   = @include $file;
+    $content    = trim(ob_get_clean()); // trim helps return ?: null (no opinion, significant whitespaces are in tags)
 
     return is_callable($callable) ? $callable : ($content ?: null);
 }
 
 function io_other(string $one)
 {
-    $io = realpath(dirname($one))     ?: throw new RuntimeException('IO Root Reality Rescinded', 500);
-
-    $ios = glob($io . '/*', GLOB_ONLYDIR)   ?: [];
-    count($ios) === 2                       || throw new RuntimeException('One folder containing in (route) and out (render) files', 500);
-
-    $other = $ios[0] === $one ? $ios[1] : $ios[0];
-    $other = is_readable($other) ? $other   : throw new RuntimeException('Other folder Not Readable', 500);
-
-    return $other;
+    return true                                         // return other child if
+        && ($io = realpath(dirname($one)))              //      the parent is real
+        && ($ios = glob($io . '/*', GLOB_ONLYDIR))      //      the parent has children
+        && isset($ios[0], $ios[1]) && !isset($ios[2])   //      exactly two children
+        ? ($ios[0] === $one ? $ios[1] : $ios[0])
+        : throw new RuntimeException('IO Other Reality Rescinded', 500);
 }
 
-function io_scaffold($addbad_scaffold_mode = 'in'): string
-{
-    ob_start(); {
-        require_once 'add/dad/scaffold.php';
-    }
-    return  ob_get_clean();  // Scaffold response
-}
