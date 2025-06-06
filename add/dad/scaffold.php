f5b03cb0b46f0d87405c91dfec54021e381bb56c
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-06 03:06:15 +0200

    io_state() instead of keep [0] + flag combination

--- add/dad/scaffold.php
+++ add/dad/scaffold.php
@@ -5,27 +5,33 @@
 <head>
     <meta charset="UTF-8">
     <title>Scaffold</title>
+    <style>
+        dt:nth-child(2n) {
+            margin-bottom: 1rem;
+            color:darkgray;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Missing <?= $in_or_out === 'in' ? 'route' : 'render' ?>  end point <?= $quest['path'] ?></h1>
+    <h1>Missing <?= (io_state($quest) & IO_IN) ? 'render' : 'route' ?> end point <?= $quest[IO_PATH] ?></h1>
     <span>Choose file to create in: <strong><?= realpath(__DIR__ . '/../io/route') ?>/</strong></span>
 
     <dl>
-        <?php
-        foreach ($quest['map'] as $depth => $handler_and_args) {
+        <?php foreach ($quest[IO_MAP] as $depth => $checkpoint): ?>
+            <dt><strong><?= htmlspecialchars($checkpoint[0]); ?></strong></dt>
+        <?php endforeach; ?>
+        <dd>
+            <?php
             list($handler, $args) = $handler_and_args;
             $handlerArgs = empty($args) ? 'no arguments' : "Expected arguments: '" . implode(',', $args) . "'";
             $templateCode = "<?php\n// $handlerArgs\nreturn function (\$quest) {\n\treturn ['status' => 200, 'body' => __FILE__];\n};";
-        ?>
-            <dt><strong><?= htmlspecialchars($handler); ?></strong></dt>
-            <dd>
-                <pre><?= htmlspecialchars($templateCode) ?: '' ?></pre>
-            </dd>
-        <?php
-        }
-        ?>
+            ?>
+            <pre><?= htmlspecialchars($templateCode) ?: '' ?></pre>
+        </dd>
     </dl>
+
+    <?php vd($quest, 'quest');?>
 </body>
 
 </html>
