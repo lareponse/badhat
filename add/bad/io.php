b11c18651dc3c59c74700b658cd07c29e39f218d
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-06 04:52:14 +0200

    reorganized, renamed, under 100 lines. completed

--- add/bad/io.php
+++ add/bad/io.php
@@ -1,33 +1,50 @@
 <?php
+
 declare(strict_types=1);
 
-const IO_GUARD        = 1;      // Init flags
-const IO_DRAFT        = 2;
+const IO_PATH = 1;      // Init flags
+const IO_PLAN = 2;
 
-const IO_IN           = 4;      // Phase flags
-const IO_OUT          = 8;
-const IO_HTTP         = 16;
+const IO_SEEK = 4;      // Phase flags
+const IO_SEND = 8;
+const IO_HTTP = 16;
 
-const IO_FILE         = 32;     // Operation flags
-const IO_ARGS         = 64;
-const IO_FUNC         = 128;
-const IO_LOAD         = 256;
+const IO_FILE = 32;     // Operation flags
+const IO_ARGS = 64;
+const IO_FUNC = 128;
+const IO_LOAD = 256;
 
 function io(string $route, string $render, $when_empty = 'index')
 {
     $path = parse_url($_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH) ?? '';
-    $quest[IO_GUARD] = io_guard($path);
-    $quest[IO_DRAFT] = io_draft($quest[IO_GUARD], $when_empty);
+    $quest[IO_PATH] = io_guard($path);
+    $quest[IO_PLAN] = io_draft($quest[IO_PATH], $when_empty);
 
-    foreach (io_probe($route, $quest) as $flag => $value) $quest[IO_IN | $flag] = $value;
-    $quest[IO_IN] = true;
+    foreach (io_probe($route, $quest) as $flag => $value) $quest[IO_SEEK | $flag] = $value;
+    $quest[IO_SEEK] = true;
 
-    if (io_state($quest) & IO_HTTP) return $quest; // quest is ready for HTTP response
-
-    foreach (io_probe($render, $quest) as $flag => $value) $quest[IO_OUT | $flag] = $value;
-    $quest[IO_OUT] = true;
+    if (!(io_state($quest) & IO_HTTP)) // quest is ready for HTTP response
+        foreach (io_probe($render, $quest) as $flag => $value) $quest[IO_SEND | $flag] = $value;
 
     return $quest;
+}
+
+function io_guard(string $coded, $max_length = 4096, $max_decode = 9, $rx_remove = '#[^A-Za-z0-9\/\.\-\_]+#'): string
+{
+    do {
+        $path = rawurldecode($coded);
+    } while ($max_decode-- > 0 && $path !== $coded && ($coded = $path));
+
+    $max_decode                       ?: throw new DomainException('Path decoding loop detected', 400);
+    (strlen($path) > $max_length)  && throw new DomainException('Path Exceeds Maximum Allowed', 400);
+
+    $path = $rx_remove ? preg_replace($rx_remove, '', $path) : $path;       // removes non alphanum /.-_
+    $path = preg_replace('#\.\.+#', '', $path);                             // remove serial dots
+    $path = preg_replace('#(?:\./|/\.|/\./)#', '/', $path);                 // replace(/): /. ./ /./
+    $path = preg_replace('#\/\/+#', '/', $path);                            // replace(/): //+, 
+    $path = trim($path, '/');                                               // remove leading and trailing slashes
+
+    return $path;
 }
 
 function io_draft(string $path, string $when_empty): array
@@ -47,15 +64,9 @@ function io_draft(string $path, string $when_empty): array
     return $candidates;
 }
 
-function io_state($quest, $__state = 0): int
-{
-    foreach ($quest as $step => $v) $__state |= $step;
-    return $__state;
-}
-
 function io_probe($start, $quest, $__mission = []): array
 {
-    foreach ($quest[IO_DRAFT] as $checkpoint)
+    foreach ($quest[IO_PLAN] as $checkpoint)
         if ($yield = io_fetch($start . $checkpoint[0])) {
 
             $__mission[IO_FILE] = $checkpoint[0];
@@ -70,7 +81,6 @@ function io_probe($start, $quest, $__mission = []): array
 
             return $__mission;
         }
-
     return $__mission;
 }
 
@@ -79,24 +89,11 @@ function io_fetch(string $file)
     ob_start();
     $callable   = @include $file;
     $content    = trim(ob_get_clean()); // trim helps return ?: null (no opinion, significant whitespaces are in tags)
-
     return is_callable($callable) ? $callable : ($content ?: null);
 }
 
-function io_guard(string $coded, $max_length = 4096, $max_decode = 9, $rx_remove = '#[^A-Za-z0-9\/\.\-\_]+#'): string
+function io_state($quest, $__state = 0): int
 {
-    do {
-        $path = rawurldecode($coded);
-    } while ($max_decode-- > 0 && $path !== $coded && ($coded = $path));
-    
-    $max_decode                       ?: throw new DomainException('Path decoding loop detected', 400);
-    (strlen($path) > $max_length)  && throw new DomainException('Path Exceeds Maximum Allowed', 400);
-
-    $path = $rx_remove ? preg_replace($rx_remove, '', $path) : $path;       // removes non alphanum /.-_
-    $path = preg_replace('#\.\.+#', '', $path);                             // remove serial dots
-    $path = preg_replace('#(?:\./|/\.|/\./)#', '/', $path);                 // replace(/): /. ./ /./
-    $path = preg_replace('#\/\/+#', '/', $path);                            // replace(/): //+, 
-    $path = trim($path, '/');                                               // remove leading and trailing slashes
-
-    return $path;
+    foreach ($quest as $step => $v) $__state |= $step;
+    return $__state;
 }
