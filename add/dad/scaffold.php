a2ea2cd6bb70c978820d68987c156300766b808b
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-07 04:15:22 +0200

    use IO_SEEK instead of IO_IN and rename IO_MAP to IO_PLAN

--- add/dad/scaffold.php
+++ add/dad/scaffold.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
-
 <head>
     <meta charset="UTF-8">
     <title>Scaffold</title>
@@ -12,13 +10,12 @@
         }
     </style>
 </head>
-
 <body>
-    <h1>Missing <?= (io_state($quest) & IO_IN) ? 'render' : 'route' ?> end point <?= $quest[IO_PATH] ?></h1>
+    <h1>Missing <?= (io_state($quest) & IO_SEEK) ? 'render' : 'route' ?> end point <?= $quest[IO_PATH] ?></h1>
     <span>Choose file to create in: <strong><?= realpath(__DIR__ . '/../io/route') ?>/</strong></span>
 
     <dl>
-        <?php foreach ($quest[IO_MAP] as $depth => $checkpoint): ?>
+        <?php foreach ($quest[IO_PLAN] as $depth => $checkpoint): ?>
             <dt><strong><?= htmlspecialchars($checkpoint[0]); ?></strong></dt>
         <?php endforeach; ?>
         <dd>
