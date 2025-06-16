db38cc9960577c74a964e67f5a4af1169bd8e601
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-16 23:48:37 +0200

    clean up code

--- add/bad/error.php
+++ add/bad/error.php
@@ -1,41 +1,38 @@
 <?php
-
-declare(strict_types=1);
-
+// no need for declare(strict_types=1);
 error_reporting(E_ALL);
 
 set_error_handler(function ($errno, $errstr) {
-    return error_log(sprintf("ADDBAD [ER:%d] : %s (%s)", $errno, $errstr, get_error_id()));
+    return error_log(sprintf("ADDBAD [ER:%d] : %s (%s)", $errno, $errstr, __random_error_id()));
 });
 
 set_exception_handler(function (Throwable $e) {
-    $message = sprintf("ADDBAD [%s:%d] : %s (%s)", get_class($e), $e->getCode(), $e->getMessage(), get_error_id());
+    $message = sprintf("ADDBAD [%s:%d] : %s (%s)", get_class($e), $e->getCode(), $e->getMessage(), __random_error_id());
     error_log(sprintf("%s in %s:%d", $message, $e->getFile(), $e->getLine()));
 
     if (function_exists('http') && !headers_sent()) {
-        // vd(0, $e);
-        // test getCode return value >99 and <600
-        $code = $e->getCode() >= 100 && $e->getCode() <= 500 ? (int)$e->getCode(): 500;
+        $code = $e->getCode() >= 100 && $e->getCode() <= 500 ? (int)$e->getCode() : 500;
         http($code, $message);
+        exit;
     }
 });
 
 register_shutdown_function(function () {
     $err = error_get_last();
     if ($err && in_array($err['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR], true)) {
-        $message = sprintf("ADDBAD [SHUTDOWN:%d] : %s (%s)", $err['type'], $err['message'], get_error_id());
+        $message = sprintf("ADDBAD [SHUTDOWN:%d] : %s (%s)", $err['type'], $err['message'], __random_error_id());
         error_log(sprintf("%s in %s:%d", $message, $err['file'], $err['line']));
 
         if (function_exists('http') && !headers_sent())
             http(500, $message);
         else
             echo PHP_SAPI === 'cli' ? $message : "<pre>$message</pre>";
-        
+
         exit(1);
     }
 });
 
-function get_error_id(): string
+function __random_error_id(): string
 {
     return base_convert((string)random_int(100000, 999999), 10, 36);
 }
