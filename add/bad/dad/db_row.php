fae4df33187538d9733c70a44adcdea1c4fab94a
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-20 12:51:45 +0200

    add db_row.php for row management functions and constants

---
+++ add/bad/dad/db_row.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+require_once 'add/bad/db.php';
+require_once 'add/bad/dad/qb.php';
+
+const ROW_ID     = 'id';
+
+const ROW_TABLE = 1;
+const ROW_SAVED = 2;
+const ROW_ALTER = 4;
+const ROW_EXTRA = 8;
+const ROW_FIELD = 16;
+
+const ROW_IMPORT = 32;
+const ROW_EXPORT = 64;
+const ROW_RELOAD = 128;
+
+const ROW_PERSIST = 256;
+
+
+function row_innit(string $table, array $data = [], int $init = ROW_ALTER): array
+{
+    $init & (ROW_SAVED | ROW_ALTER | ROW_EXTRA) || throw new BadFunctionCallException('db_row_innit requires ROW_SAVED, ROW_ALTER or ROW_EXTRA', 500);
+    return [ROW_TABLE => $table, $init => $data];
+}
+
+function row($row, int $behave = ROW_IMPORT, array $boat = [])
+{
+    if ($behave & ROW_RELOAD) {
+        $st = dbq(db(), ...qb_read($row[ROW_TABLE], $boat));
+        $st->rowCount() === 1 || throw new BadFunctionCallException('db_row ROW_RELOAD yields rowCount() !==1', 500);
+        $row[ROW_SAVED] = $st->fetch(PDO::FETCH_ASSOC) ?: false;
+    }
+
+    if ($behave & ROW_FIELD && empty($row[ROW_FIELD])) {
+        $row[ROW_FIELD] = $boat ?: row_schema($row);
+        return $row;
+    }
+
+    if ($behave & ROW_IMPORT) {
+
+        foreach ($boat as $col => $value)
+            if (!$row[ROW_SAVED] || !isset($row[ROW_SAVED][$col]) || $row[ROW_SAVED][$col] !== $value)
+                if ($row[ROW_FIELD] && isset($row[ROW_FIELD][$col]))
+                    $row[ROW_ALTER][$col] = $value;
+                else
+                    $row[ROW_EXTRA][$col] = $value;
+        return $row;
+    }
+
+    if ($behave & ROW_PERSIST && $row[ROW_ALTER]) {
+        $row[ROW_PERSIST] = dbq(db(), ...($row[ROW_SAVED]
+            ? qb_update($row[ROW_TABLE], $row[ROW_ALTER], ...qb_where(['id' => $row[ROW_SAVED]['id']]))
+            : qb_create($row[ROW_TABLE], $row[ROW_ALTER], $row[ROW_FIELD])));
+        return $row;
+    }
+
+    if ($behave & ROW_EXPORT) {
+        $ret = [];
+        foreach ($boat as $col)
+            $ret[$col] = $row[ROW_ALTER][$col] ?? $row[ROW_SAVED][$col];
+
+        return $ret;
+    }
+    
+    return $row;
+}
+
+function row_schema($row): array
+{
+    $pdo = db();
+    $table = $row[ROW_TABLE];
+
+    if ($row[ROW_FIELD])
+        return $row[ROW_FIELD];
+
+    if ($row[ROW_SAVED] && $row[ROW_SAVED] !== false)
+        return $row[ROW_SAVED];
+
+    $fields = [];
+    $fields_query = dbq($pdo, "SELECT * FROM `$table` LIMIT 1");
+    if (!$fields_query->rowCount() && $fields_query->columnCount() > 0)
+        for ($i = 0, $cnt = $fields_query->columnCount(); $i < $cnt; ++$i)
+            $fields[] = $fields_query->getColumnMeta($i)['name'];
+
+    return array_flip($fields);
+}
