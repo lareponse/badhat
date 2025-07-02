945aa3efd2ca26a752f235479621a75609849f1c
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-07-02 23:26:34 +0200

    fix(scaffold): update http guard function to use http_in

--- add/bad/dad/scaffold.php
+++ add/bad/dad/scaffold.php
@@ -1,13 +1,12 @@
-Missing <?= ($quest[QST_CORE] & QST_PULL) ? 'render' : 'route' ?> end point <?= http_guard() ?>
+Missing <?= ($quest[QST_CORE] & QST_PULL) ? 'render' : 'route' ?> end point <?= http_in() ?>
 
 
 Choose file to create in: <?= realpath(__DIR__ . '/../../../app/io/route') ?>
 
-<?php foreach ((chart(io_guard(http_guard()))) as $handler => $args): ?>
+<?php foreach ((io_route(http_in())) as $handler => $args): ?>
 <?= PHP_EOL.htmlspecialchars($handler); ?>
     <?php
     $handlerArgs = empty($args) ? 'no arguments' : "Expected arguments: '" . implode(',', $args) . "'";
     $templateCode = "<?php\n// $handlerArgs\nreturn function (\$quest) {\n\treturn ['status' => 200, 'body' => __FILE__];\n};";
     ?>
-
 <?php endforeach; ?>
