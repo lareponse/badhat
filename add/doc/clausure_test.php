1b3b92604a33c6aa9329caf319762ad2d962ba48
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-07-03 03:49:30 +0200

    testing with new fresh closure, reusing is still to be tested and controlled

--- add/doc/clausure_test.php
+++ add/doc/clausure_test.php
@@ -1,11 +1,11 @@
 <?php
 
-require 'add/bad/dad/clausure.php';
-require 'add/test.php';
+require 'add/bad/clausure.php';
+require 'add/bad/test.php';
 
 
 return function () {
-
+    // clause(CLAUSE_SELECT)
     test('select: single comma-string', function () {
         list($sql, $bind) = clause(CLAUSE_SELECT)('id,COUNT(*),username,email');
         assert($sql === 'SELECT id,COUNT(*),username,email', 'wrong SQL');
@@ -24,95 +24,93 @@ return function () {
         assert($bind === [], 'bindings should be empty');
     });
 
-
-    $where = clause(CLAUSE_WHERE, '=');
-    test('where: single condition', function () use ($where) {
-        list($sql, $bind) = $where("id = 1 AND username = 'test'");
+    // clause(CLAUSE_WHERE, '=')
+    test('where: single condition', function () {
+        list($sql, $bind) = clause(CLAUSE_WHERE, '=')("id = 1 AND username = 'test'");
         assert($sql === 'WHERE id = 1 AND username = \'test\'', 'wrong SQL');
         assert($bind === [], 'bindings should be empty');
     });
-
-    test('where: multiple conditions', function () use ($where) {
-        list($sql, $bind) = $where("id = 1", "username = 'test'");
+    test('where: multiple conditions', function () {
+        list($sql, $bind) = clause(CLAUSE_WHERE, '=')("id = 1", "username = 'test'");
         assert($sql === 'WHERE id = 1 AND username = \'test\'', 'wrong SQL');
         assert($bind === [], 'bindings should be empty');
     });
-
-    test('where: associative array', function () use ($where) {
-        list($sql, $bind) = $where(['id' => 1, 'username' => 'test']);
+    test('where: associative array', function () {
+        list($sql, $bind) = clause(CLAUSE_WHERE, '=')(['id' => 1, 'username' => 'test']);
         assert($sql === 'WHERE `id` = :id AND `username` = :username', 'wrong SQL');
         assert($bind === ['id' => 1, 'username' => 'test'], 'bindings should be empty');
     });
 
-    $where = clause(CLAUSE_WHERE | OP_OR, '=');
-    test('where: OR single condition', function () use ($where) {
-        list($sql, $bind) = $where("id = 1 OR username = 'test'");
+    // clause(CLAUSE_WHERE | OP_OR, '=')
+    test('where: OR single condition', function () {
+        list($sql, $bind) = clause(CLAUSE_WHERE | OP_OR, '=')("id = 1 OR username = 'test'");
         assert($sql === 'WHERE id = 1 OR username = \'test\'', 'wrong SQL');
         assert($bind === [], 'bindings should be empty');
     });
-
-    test('where: OR multiple conditions', function () use ($where) {
-        list($sql, $bind) = $where("id = 1", "username = 'test'");
+    test('where: OR multiple conditions', function () {
+        list($sql, $bind) = clause(CLAUSE_WHERE | OP_OR, '=')("id = 1", "username = 'test'");
         assert($sql === 'WHERE id = 1 OR username = \'test\'', 'wrong SQL');
         assert($bind === [], 'bindings should be empty');
     });
-
-    test('where: OR associative array', function () use ($where) {
-        list($sql, $bind) = $where(['id' => 1, 'username' => 'test']);
+    test('where: OR associative array', function () {
+        list($sql, $bind) = clause(CLAUSE_WHERE | OP_OR, '=')(['id' => 1, 'username' => 'test']);
         assert($sql === 'WHERE `id` = :id OR `username` = :username', 'wrong SQL');
         assert($bind === ['id' => 1, 'username' => 'test'], 'bindings should be empty');
     });
 
 
-    $and = clause(OP_AND, '=');
-    test('and: single condition', function () use ($and) {
-        list($sql, $bind) = $and("id = 1 AND username = 'test'");
+    // clause(OP_AND, '=');
+    test('and: single condition', function () {
+        list($sql, $bind) = clause(OP_AND, '=')("id = 1 AND username = 'test'");
         assert($sql === '(id = 1 AND username = \'test\')', 'wrong SQL');
         assert($bind === [], 'bindings should be empty');
     });
-    test('and: multiple conditions', function () use ($and) {
-        list($sql, $bind) = $and("id = 1", "username = 'test'");
+    test('and: multiple conditions', function () {
+        list($sql, $bind) = clause(OP_AND, '=')("id = 1", "username = 'test'");
+        vd($sql);
+
         assert($sql === '(id = 1 AND username = \'test\')', 'wrong SQL');
         assert($bind === [], 'bindings should be empty');
     });
-    test('and: associative array', function () use ($and) {
-        list($sql, $bind) = $and(['id' => 1, 'username' => 'test']);
+    test('and: associative array', function () {
+        list($sql, $bind) = clause(OP_AND, '=')(['id' => 1, 'username' => 'test']);
         assert($sql === '(`id` = :id AND `username` = :username)', 'wrong SQL');
         assert($bind === ['id' => 1, 'username' => 'test'], 'bindings should be empty');
     });
 
-    $or = clause(OP_OR, '=');
-    test('or: single condition', function () use ($or) {
-        list($sql, $bind) = $or("id = 1 OR username = 'test'");
+    // clause(OP_OR, '=');
+    test('or: single condition', function () {
+        list($sql, $bind) = clause(OP_OR, '=')("id = 1 OR username = 'test'");
         assert($sql === '(id = 1 OR username = \'test\')', 'wrong SQL');
         assert($bind === [], 'bindings should be empty');
     });
-    test('or: multiple conditions', function () use ($or) {
-        list($sql, $bind) = $or("id = 1", "username = 'test'");
+    test('or: multiple conditions', function () {
+        list($sql, $bind) = clause(OP_OR, '=')("id = 1", "username = 'test'");
         assert($sql === '(id = 1 OR username = \'test\')', 'wrong SQL');
         assert($bind === [], 'bindings should be empty');
     });
-    test('or: associative array', function () use ($or) {
-        list($sql, $bind) = $or(['id' => 1, 'username' => 'test']);
+    test('or: associative array', function () {
+        list($sql, $bind) = clause(OP_OR, '=')(['id' => 1, 'username' => 'test']);
         assert($sql === '(`id` = :id OR `username` = :username)', 'wrong SQL');
         assert($bind === ['id' => 1, 'username' => 'test'], 'bindings should be empty');
     });
 
-
-    test('and with nested or', function () use ($and, $or) {
-        list($sql, $bind) = $and(
+    // clause(OP_AND, '=')
+    // clause(OP_OR, '=')
+    test('and with nested or', function () {
+        list($sql, $bind) = clause(OP_AND, '=')(
             "id = 1",
-            $or("username = 'test'", "email = 'test@test.com'"),
+            clause(OP_OR, '=')("username = 'test'", "email = 'test@test.com'"),
             "status = 'active'"
         );
         assert($sql === '(id = 1 AND (username = \'test\' OR email = \'test@test.com\') AND status = \'active\')', 'wrong SQL');
         assert($bind === [], 'bindings should be empty');
     });
 
-    test('and with nested or', function () use ($and, $or) {
-        list($sql, $bind) = $and(
+    test('and with nested or assoc', function () {
+        list($sql, $bind) = clause(OP_AND, '=')(
             ['id' => 1],
-            $or(['username' => 'test', "email" => 'test@test.com']),
+            clause(OP_OR, '=')(['username' => 'test', "email" => 'test@test.com']),
             ['status' => 'active']
         );
         vd($sql);
@@ -134,7 +132,6 @@ return function () {
     // $or = clause(OP_OR);
     // $in = clause(PH_LIST|OP_IN);
     // vd($in([3, 4]));
-    // $query = clause(CLAUSE_QUERY);
     // $order = clause(CLAUSE_ORDER_BY);
 
     // // vd(clause(OP_IN)("tag_id", [3, 4]));
