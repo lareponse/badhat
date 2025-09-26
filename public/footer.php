bc8947e61746030d36d76c20037640090afa442c
Lukasz Gryczewski <luka.gryczewski@gmail.com>
2025-09-26 14:56:48 +0200

    Create footer.php

---
+++ public/footer.php
@@ -0,0 +1,48 @@
+<footer>
+    <section>
+        <h2>Coordonnées</h2>
+        <address>
+            <p><strong>IRSA - Institut Royal pour Sourds et Aveugles</strong></p>
+            <p>Chaussée de Waterloo 150,<br>
+                1180 Uccle - Belgique</p>
+        </address>
+    </section>
+
+    <section>
+        <h2>Liens utiles</h2>
+        <ul>
+            <li><a href="">À propos de l'IRSA</a></li>
+            <li><a href="">Nos services</a></li>
+            <li><a href="">Faire un don</a></li>
+            <li><a href="">Rejoindre nos équipes</a></li>
+            <li><a href="contact_form.html">Contact</a></li>
+            <li><a href="">Plan du site</a></li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Mentions et accessibilité</h2>
+        <ul>
+            <li><a href="">Mentions légales</a></li>
+            <li><a href="">Politique de confidentialité</a></li>
+            <li><a href="">Accessibilité du site</a></li>
+            <li><a href="">Cookies</a></li>
+            <li><a href="">Gestion des données personnelles</a></li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Réseaux sociaux</h2>
+        <ul>
+            <li><a href="https://www.facebook.com/InstitutRoyalpourSourdsetAveugles/?locale=fr_FR" aria-label="Suivez l'IRSA sur Facebook">Facebook</a></li>
+            <li><a href="https://www.instagram.com/irsa.bruxelles/?hl=fr" aria-label="Suivez l'IRSA sur Instagram">Instagram</a></li>
+            <li><a href="https://be.linkedin.com/company/irsa-institut-royal-pour-sourds-et-aveugles" aria-label="Suivez l'IRSA sur LinkedIn">LinkedIn</a></li>
+            <li><a href="https://www.youtube.com/channel/UCm2liEWa1lxZUQRZLRW3MWg" aria-label="Suivez l'IRSA sur YouTube">YouTube</a></li>
+        </ul>
+    </section>
+
+    <section>
+        <p>&copy; IRSA - Institut Royal pour Sourds et Aveugles - Tous droits réservés</p>
+        <p><em>Site réalisé par Z.Kiss, S.Makina, L.Gryczewski</em></p>
+    </section>
+</footer>
