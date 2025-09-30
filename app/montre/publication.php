54160e129f55cada08019c615ba962f11a132094
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-09-30 05:03:17 +0200

    added publication page

---
+++ app/montre/publication.php
@@ -0,0 +1,178 @@
+<header>
+    <h1><span class="tight">Les Publications de l'IRSA</span></h1>
+    <div class="tight">
+        <p>Pour toute commande, merci de contacter Julien Collart au <a href="tel:+3228826900">02/882 69 00</a> - <a href="mailto:documentation@irsa.be">documentation@irsa.be</a></p>
+        <p><a href="/ui/publication/pdf/brochures/brochure-collection-sensorialites.pdf" target="_blank" rel="noopener">Découvrez notre collection <em>Sensorialités</em></a></p>
+    </div>
+</header>
+
+<section aria-labelledby="nouveautes" class="tight">
+    <h2 id="nouveautes">Nouveautés</h2>
+
+    <div class="card-grid">
+        <article>
+            <img src="/ui/publication/sensorialites-prebraille.jpg" alt="Du prébraille au braille" width="300" height="428">
+            <div>
+                <a href="https://pul.uclouvain.be/book/?gcoi=29303100296900" target="_blank" rel="noopener">Du prébraille au braille</a>
+                <p>Apprentissage de la lecture et de l'écriture chez l'enfant déficient visuel</p>
+                <p>Coordonné par Anne Bragard</p>
+                <ul class="publication-formats">
+                    <li>20€ (version imprimée)</li>
+                    <li>13€ (version numérique)</li>
+                </ul>
+            </div>
+        </article>
+
+        <article>
+            <img src="/ui/publication/Premier_pas_avec_eft_df_auditif.jpg" alt="Premiers pas avec un enfant déficient auditif" width="300" height="440">
+            <div>
+                <a href="https://pul.uclouvain.be/book/?gcoi=29303100792300" target="_blank" rel="noopener">Premiers pas avec un enfant déficient auditif</a>
+                <p>Regards sur la surdité</p>
+                <p>Coordonné par Anne Bragard</p>
+                <ul class="publication-formats">
+                    <li>28,5€ (version imprimée)</li>
+                    <li>19€ (version numérique)</li>
+                </ul>
+            </div>
+        </article>
+    </div>
+</section>
+
+<section aria-labelledby="collection" class="tight">
+    <h2 id="collection">Collection Sensorialités</h2>
+
+    <div class="card-grid">
+        <article>
+            <img src="/ui/publication/99224-PUL-Deprez-identitesourde-COV-WEB.jpg" alt="Identité sourde et implant cochléaire" width="300" height="442">
+            <div>
+                <p>Vers une identité plurielle</p>
+                <a href="#" target="_blank" rel="noopener">Identité sourde et implant cochléaire</a>
+                <p>par Alice Leidensdorf</p>
+            </div>
+        </article>
+
+        <article>
+            <img src="/ui/publication/Aka-nouvelle-dition.jpg" alt="L'AKA - Alphabet des Kinèmes Assistés" width="300" height="428">
+            <div>
+                <p>Alphabet des Kinèmes Assistés</p>
+                <a href="https://pul.uclouvain.be/book/?gcoi=29303100161080" target="_blank" rel="noopener">L'AKA</a>
+                <p>par Walter Wouts - 25€</p>
+                <a href="/ui/publication/Aka01.mp4">Extrait carte USB</a>
+            </div>
+        </article>
+
+        <article>
+            <img src="/ui/publication/Cover1GVFvol1.jpg" alt="Grammaire visuelle du français vol.1" width="300" height="428">
+            <div>
+                <p>Approche théorique - par René Cerise</p>
+                <a href="https://pul.uclouvain.be/livre/?GCOI=29303100532400" target="_blank" rel="noopener">Grammaire visuelle du français pour l'enfant sourd</a>
+                <ul class="publication-formats">
+                    <li>26€ (version imprimée)</li>
+                    <li>17€ (version numérique)</li>
+                </ul>
+            </div>
+        </article>
+
+        <article>
+            <img src="/ui/publication/Cover1GVFvol2.jpg" alt="Manuel du praticien" width="300" height="425">
+            <div>
+                <p>par Sébastien Soers</p>
+                <a href="https://pul.uclouvain.be/book/?gcoi=29303100521530" target="_blank" rel="noopener">Manuel du praticien</a>
+                <ul class="publication-formats">
+                    <li>22€ (version imprimée)</li>
+                    <li>14€ (version numérique)</li>
+                </ul>
+            </div>
+        </article>
+
+        <article>
+            <img src="/ui/publication/Cover1GVFvol3.jpg" alt="Outils didactiques" width="300" height="423">
+            <div>
+                <p>par Sébastien Soers</p>
+                <a href="https://pul.uclouvain.be/book/?gcoi=29303100942450" target="_blank" rel="noopener">Outils didactiques</a>
+            </div>
+        </article>
+
+        <article>
+            <img src="/ui/publication/couv_ccitesurditetdl.jpg" alt="Cécité, surdité et troubles du langage" width="300" height="440">
+            <div>
+                <p>Approches cliniques, juridiques et pédagogiques</p>
+                <a href="https://pul.uclouvain.be/book/?gcoi=29303100744340" target="_blank" rel="noopener">Cécité, surdité et troubles du langage</a>
+                <p>Coordonné par Anne Bragard</p>
+                <ul class="publication-formats">
+                    <li>23€ (version imprimée)</li>
+                    <li>15€ (version numérique)</li>
+                </ul>
+            </div>
+        </article>
+
+        <article>
+            <figure>
+
+                <img src="/ui/publication/Couv-sensorialit-deficiencevisuelle.jpg" alt="Premiers pas avec une personne déficiente visuelle" width="300" height="440">
+            </figure>
+            <div>
+                <p>Boîte à outils - par Anne Bragard</p>
+                <a href="https://pul.uclouvain.be/book/?gcoi=29303100481590" target="_blank" rel="noopener">Premiers pas avec une personne déficiente visuelle</a>
+                <ul class="publication-formats">
+                    <li>17€ (version imprimée)</li>
+                    <li>11€ (version numérique)</li>
+                </ul>
+            </div>
+        </article>
+
+        <article>
+            <figure>
+                <img src="/ui/publication/premier-pas-visuel-en.jpg" alt="First steps with a visually impaired person" width="300" height="429">
+            </figure>
+            <div>
+                <p>Toolbox - Coordinated by Anne Bragard</p>
+                <a href="https://pul.uclouvain.be/book/?gcoi=29303100621400" target="_blank" rel="noopener">First steps with a visually impaired person</a>
+                <p>Translated by Marcel Crochet</p>
+                <ul class="publication-formats">
+                    <li>18€ (printed version)</li>
+                    <li>12€ (digital version)</li>
+                </ul>
+            </div>
+        </article>
+    </div>
+</section>
+
+<section aria-labelledby="autres" class="tight">
+    <h2 id="autres">Autres publications</h2>
+
+    <div class="card-grid">
+        <article>
+            <img src="/ui/publication/EnsSpecCover.jpg" alt="L'enseignement spécialisé" width="300" height="421">
+            <div>
+                <h3>L'enseignement spécialisé</h3>
+                <p>Enjeux et fondements</p>
+                <p>par Marcel Crochet - 2€</p>
+            </div>
+        </article>
+
+        <article>
+            <img src="/ui/publication/irsa-175-ans.jpg" alt="Histoire des 175 ans de l'IRSA" width="300" height="300">
+            <div>
+                <h3>Histoire des 175 ans de l'IRSA</h3>
+                <p>5€</p>
+            </div>
+        </article>
+
+        <article>
+            <img src="/ui/publication/SadRegCover.jpg" alt="S'adresser à un regard" width="300" height="356">
+            <div>
+                <h3>S'adresser à un regard</h3>
+                <p>Enseigner à l'enfant sourd - Essai</p>
+                <p>par Christine Hamoir - 10€</p>
+            </div>
+        </article>
+    </div>
+</section>
+
+
+<?php
+return function ($this_html, $args) {
+    [$ret, $page] = ob_ret_get('app/montre/layout.php', ['main' => $this_html, 'css' => ['page-publication']], IO_EXTRACT);
+    return $page;
+};
