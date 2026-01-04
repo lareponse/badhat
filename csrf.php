bf1ec43c1245d2c1e0a8cff8cad58d4d68e5aa56
HexMakina <20891495+HexMakina@users.noreply.github.com>
2026-01-04 12:33:14 +0100

    using namespace for session keys and strengthen SETUP

--- csrf.php
+++ csrf.php
@@ -1,34 +1,36 @@
 <?php
+namespace badhat\csrf;
+
 const CSRF_SETUP = 1;
 const CSRF_CHECK = 2;
-const CSRF_INPUT = 4 | CSRF_SETUP;
+const CSRF_INPUT = 4;
 
-function csrf(int $behave = 0, $param = null, $k = null)
+function csrf(string $name, int $behave, $param = null)
 {
-    static $key = '_csrf_token';
-
-    session_status() === PHP_SESSION_ACTIVE || throw new RuntimeException('no active session for csrf', 500);
+    session_status() === PHP_SESSION_ACTIVE     || throw new RuntimeException('No active session for CSRF', 500);
+    $name                                       ?: throw new InvalidArgumentException('CSRF name required', 500);
 
     if (CSRF_SETUP & $behave) {
-        is_string($k) && $k && ($key = $k);
-        $ttl = $param ?? 3600;
+        $ttl = is_int($param) && $param > 0 ? $param : 3600;
         $now = time();
-        (!isset($_SESSION[$key][1]) || $now > $_SESSION[$key][1])
-        && $_SESSION[$key] = [bin2hex(random_bytes(32)), $now + $ttl];
 
-        $param = null;
+        if (!isset($_SESSION[__NAMESPACE__][$name][1]) || $now > $_SESSION[__NAMESPACE__][$name][1])
+            $_SESSION[__NAMESPACE__][$name] = [bin2hex(random_bytes(32)),$now + $ttl];
     }
 
-    if (CSRF_CHECK & $behave) {
-        [$master, $exp] = $_SESSION[$key] ?? throw new BadFunctionCallException('CSRF not initialized', 403);
+    $_SESSION[__NAMESPACE__][$name]          ?? throw new BadFunctionCallException("CSRF '$name' not initialized", 403);
 
-        $token = (is_string($param) ? $param : ($_POST[$key] ?? ''));
-        $token || throw new BadFunctionCallException('CSRF token required', 403);
+    if (CSRF_CHECK & $behave) {
+        [$master, $exp] = $_SESSION[__NAMESPACE__][$name];
+
+        $token = is_string($param) ? $param : ($_POST[$name] ?? '');
+        $token                                  || throw new BadFunctionCallException("CSRF '$name' token required", 403);
+
         return time() <= $exp && hash_equals($master, $token);
     }
 
     if (CSRF_INPUT & $behave)
-        return '<input type="hidden" name="' . $key . '" value="' . $_SESSION[$key][0] . '" />';
+        return '<input type="hidden" name="' . htmlspecialchars($name, ENT_QUOTES). '" value="' . $_SESSION[__NAMESPACE__][$name][0] . '" />';
 
-    return $_SESSION[$key][0] ?? null;
+    return $_SESSION[__NAMESPACE__][$name][0];
 }
