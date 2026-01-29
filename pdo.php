324bffce79df6892ca27c7082babc7f634212b83
HexMakina <20891495+HexMakina@users.noreply.github.com>
2026-01-29 11:50:38 +0100

    removed 400 and 500 exception code, was confusing and useless. Exception types might be better

--- pdo.php
+++ pdo.php
@@ -6,7 +6,7 @@ function db(?\PDO $pdo = null): \PDO
 {// get/set the shared PDO instance
     static $cache = null;                                           // request-scoped PDO cache
     ($pdo !== null) && ($cache = $pdo);                             // initialize / replace cached connection
-    return $cache                                                   ?? throw new \BadFunctionCallException(__FUNCTION__.':NO_CACHED_PDO', 400);
+    return $cache                                                   ?? throw new \BadFunctionCallException(__FUNCTION__.':NO_CACHED_PDO');
 }
 
 function qp(string $query, ?array $params = null, array $prep_options = [], ?\PDO $pdo = null): \PDOStatement
@@ -21,25 +21,25 @@ function qp(string $query, ?array $params = null, array $prep_options = [], ?\PD
 function trans(callable $transaction, ?\PDO $pdo = null)
 {// execute callable inside a single atomic transaction
     $pdo ??= db();                                                  // resolve PDO lazily from cache
-    !$pdo->inTransaction()                                          || throw new \LogicException('PDO does not support real nested transactions', 400);
+    !$pdo->inTransaction()                                          || throw new \LogicException('PDO does not support real nested transactions');
     $res = null;                                                    // capture callable result for return
     try {                                                           // outer try/catch to manage transaction atomicity
-        $pdo->beginTransaction()                                    || throw _ErrorInfoException($pdo, 'beginTransaction', 500);
+        $pdo->beginTransaction()                                    || throw _ErrorInfoException($pdo, 'beginTransaction');
         try {
             $res = $transaction($pdo);
         } catch (\Throwable $t) {
-            throw _ErrorInfoException($pdo, 'transaction callable', 400, $t);
+            throw _ErrorInfoException($pdo, 'transaction callable', 0xC0D, $t);
         }
-        $pdo->commit()                                              || throw _ErrorInfoException($pdo, 'commit', 500);
+        $pdo->commit()                                              || throw _ErrorInfoException($pdo, 'commit');
     } catch (\Throwable $t) {
-        $pdo->inTransaction() && ($pdo->rollBack()                  || throw _ErrorInfoException($pdo, 'rollback', 500, $t));
+        $pdo->inTransaction() && ($pdo->rollBack()                  || throw _ErrorInfoException($pdo, 'rollback',$t));
         throw $t;                                                   // rethrow original error after rollback
     }
     return $res;
 }// return the callable result after successful commit
 
-function _ErrorInfoException(\PDO|\PDOStatement $source, $action, $exception_code=null, ?\Throwable $chain = null): \RuntimeException
+function _ErrorInfoException(\PDO|\PDOStatement $source, $action, ?\Throwable $chain = null): \RuntimeException
 {// creates rich, chainable runtime exception
     $error = $source->errorInfo() ?: ['NO_STATE', 'NO_CODE', 'errorInfo(): empty'];
-    return new \RuntimeException("[STATE={$error[0]}, CODE={$error[1]}] PDO::{$action}() failed ({$error[2]})", $exception_code, $chain);
+    return new \RuntimeException("[STATE={$error[0]}, CODE={$error[1]}] PDO::{$action}() failed ({$error[2]})", 0xC0D, $chain);
 }// returns \RuntimeException with PDO error info embedded in message
