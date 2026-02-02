b39d05b568c6a4ac98fe9df56dedc5b1aae426d1
HexMakina <20891495+HexMakina@users.noreply.github.com>
2026-02-02 01:38:25 +0100

    url_path validation + ALPHANUM const + RFC reference

--- rfc.php
+++ rfc.php
@@ -2,24 +2,21 @@
 
 namespace bad\rfc;
 
+const ALPHANUM = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 // RFC 9110 (HTTP) alphabets (ASCII + obs-text)
-const RFC_H_TCHAR     = "!#$%&'*+-.^_`|~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; // RFC 9110 §5.1 (field name token chars)
-const RFC_H_VCHAR     = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"; // RFC 9110 §5.5 (field value VCHAR)
-const RFC_H_OBS_TEXT  = "\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF";
+const RFC_TCHAR     = ALPHANUM . "!#$%&'*+-.^_`|~";                       // RFC 9110 §5.1 (field name token chars)
+const RFC_VCHAR     = ALPHANUM . "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";    // RFC 5234
+const RFC_H_OBS_TEXT  = "\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF"; // RFC 9110 
 const RFC_H_OWS       = " \t"; // SP and HTAB
-const RFC_H_VALUE_SAFE_BYTES = RFC_H_VCHAR . RFC_H_OBS_TEXT . RFC_H_OWS;
+const RFC_H_VALUE_SAFE_BYTES = RFC_VCHAR . RFC_H_OBS_TEXT . RFC_H_OWS;
 
 // RFC 9110 §5.5; RFC 5234 App B.1 — CTL excluding HTAB (so HTAB is allowed via OWS)
-const RFC_H_CTL_NO_HTAB  =
-    "\x00\x01\x02\x03\x04\x05\x06\x07\x08"
-  . "\x0A\x0B\x0C\x0D\x0E\x0F"
-  . "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F"
-  . "\x7F";
+const RFC_H_CTL_NO_HTAB  = "\x00\x01\x02\x03\x04\x05\x06\x07\x08" . "\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F" . "\x7F";
 
 // RFC 3986 (URI) alphabets (ASCII only)
 const RFC_U_HEX        = "0123456789ABCDEFabcdef";
-const RFC_U_UNRESERVED = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
-const RFC_U_SUBDELIMS  = "!$&'()*+,;=";
+const RFC_U_UNRESERVED = ALPHANUM . "-._~"; // RFC 3986
+const RFC_U_SUBDELIMS  = "!$&'()*+,;=";     // RFC 3986
 
 const RFC_U_PCHAR      = RFC_U_UNRESERVED . RFC_U_SUBDELIMS . ":@"; // pchar = unreserved / pct-encoded / sub-delims / ":" / "@"
 const RFC_U_PATH_SAFE  = RFC_U_PCHAR . "/%";                        // path = *( "/" segment ) ; segment = *pchar
@@ -27,7 +24,7 @@ const RFC_U_PATH_SAFE  = RFC_U_PCHAR . "/%";                        // path = *(
 function field_name(string $canon): array
 {
     ($canon !== '')                                                 || throw new \InvalidArgumentException('field name is empty');
-    !isset($canon[\strspn($canon, RFC_H_TCHAR)])                    || throw new \InvalidArgumentException('field name must be a token (tchar alphabet)');
+    !isset($canon[\strspn($canon, RFC_TCHAR)])                    || throw new \InvalidArgumentException('field name must be a token (tchar alphabet)');
     return ['canon' => $canon, 'key' => \strtolower($canon)];
 }
 
@@ -42,9 +39,24 @@ function field_value(string $value, bool $allow_blank = false): string
     return $value;
 }
 
-function url_path($string $path): string
+function url_path(string $path): string
 {
     \strpbrk($path, RFC_H_CTL_NO_HTAB . RFC_H_OWS) === false        || throw new \InvalidArgumentException('path forbids CTL/SP/HTAB');
     \strpos($path, "\\") === false                                  || throw new \InvalidArgumentException('path forbids backslash');
 
+    $allowed_no_pct = RFC_U_PCHAR . "/";
+    for ($i = 0, $len = \strlen($path); $i < $len; ++$i) {
+        $current = $path[$i];
+        if ($current === '%') {
+            ($i + 2 < $len)                                         || throw new \InvalidArgumentException('path has incomplete percent-escape');
+            (\ctype_xdigit($path[$i + 1]) && \ctype_xdigit($path[$i + 2])) || throw new \InvalidArgumentException('path has invalid percent-escape');
+            $i += 2; // skip the two hex digits
+            continue;
+        }
+
+        // For any non-'%' byte, it must be in the allowed set.
+        (\strpos($allowed_no_pct, $current) !== false)              || throw new \InvalidArgumentException('path has illegal byte 0x' . \strtoupper(\bin2hex($current)));
+    }
+
+    return $path;
 }
